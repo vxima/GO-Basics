0d42700dbbe86d438f18210d3b10dfec09c5e828
vxima <vxco@cin.ufpe.br>
2022-07-29 10:58:38 -0300

    add for loop

---
+++ ForLoop/birdwatcher.go
@@ -0,0 +1,34 @@
+package birdwatcher
+
+// TotalBirdCount return the total bird count by summing
+// the individual day's counts.
+func TotalBirdCount(birdsPerDay []int) int {
+	count := 0
+	for i := 0; i < len(birdsPerDay); i++ {
+		count = count + birdsPerDay[i]
+	}
+	return count
+}
+
+// BirdsInWeek returns the total bird count by summing
+// only the items belonging to the given week.
+func BirdsInWeek(birdsPerDay []int, week int) int {
+	index := (week - 1) * 7
+	count := 0
+	for i := index; i < index+7; i++ {
+		count = count + birdsPerDay[i]
+
+	}
+	return count
+}
+
+// FixBirdCountLog returns the bird counts after correcting
+// the bird counts for alternate days.
+func FixBirdCountLog(birdsPerDay []int) []int {
+	for i := 0; i < len(birdsPerDay); i++ {
+		if i%2 == 0 {
+			birdsPerDay[i] = birdsPerDay[i] + 1
+		}
+	}
+	return birdsPerDay
+}
